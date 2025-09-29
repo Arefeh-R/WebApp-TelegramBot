1f6c4878560abe26f83e65a0f43ebef16a83aedd
Arefeh_R <arefeh.rostami.sar@gmail.com>
2025-09-29 10:10:28 +0330

    Added two models comment and  userbook

--- webapp_telegrambot/library/admin.py
+++ webapp_telegrambot/library/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-# from library.models import Author, Book, Review, Comment
+from library.models import Author, Book, Review, Comment
 
-# # Register your models here.
-# admin.site.register(Author)
-# admin.site.register(Book)
-# admin.site.register(Review)
-# admin.site.register(Comment)
+# Register your models here.
+admin.site.register(Author)
+admin.site.register(Book)
+admin.site.register(Review)
+admin.site.register(Comment)
